8d676eec0f5e0af4c2232a24260e41d1139b793b
izza <izzalutfi045@gmail.com>
2020-01-08 02:10:19 +0700

    modified produk

--- admin/page/produk.php
+++ admin/page/produk.php
@@ -26,128 +26,158 @@
                         <select name="id_kategori" class="form-control">
                           <option value="0">--Pilih Kategori--</option>
                           <?php 
-                            include '../koneksi.php';
-                            $query = mysqli_query($koneksi, "SELECT * FROM kategori");
-                            while($dk = mysqli_fetch_array($query)){
+                          include '../koneksi.php';
+                          $query = mysqli_query($koneksi, "SELECT * FROM kategori");
+                          while($dk = mysqli_fetch_array($query)){
                            ?>
                            <option value="<?=$dk['id_kategori'];?>"><?=$dk['nama_kategori'];?></option>
-                          <?php } ?>
-                        </select>
-                      </div>
-                      <div class="form-group">
-                        <label for="exampleInputEmail1">Foto</label>
-                        <input type="file" name="foto" class="form-control">
-                      </div>
-                      <div class="form-group">
-                        <label for="exampleInputEmail1">Stok</label>
-                        <input type="number" name="stok" class="form-control" placeholder="Masukkan Jumlah Stok">
-                      </div>
-                      <div class="form-group">
-                        <label for="exampleInputEmail1">Harga</label>
-                        <input type="number" name="harga" class="form-control" placeholder="Masukkan Harga Barang">
-                      </div>
-                      <div class="form-group">
-                        <label for="exampleInputEmail1">Diskon (jika ada)</label>
-                        <input type="number" name="diskon" class="form-control" value="0">
-                      </div>
-                      <div class="form-group">
-                        <label for="exampleInputEmail1">Deskripsi</label>
-                        <textarea class="form-control" name="deskripsi"></textarea>
-                      </div>
-
+                         <?php } ?>
+                       </select>
+                     </div>
+                     <div class="form-group">
+                      <label for="exampleInputEmail1">Foto</label>
+                      <input type="file" name="foto" class="form-control">
                     </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
-                      <button type="submit" class="btn btn-primary">Simpan</button>
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Stok</label>
+                      <input type="number" name="stok" class="form-control" placeholder="Masukkan Jumlah Stok">
                     </div>
-                  </form>
-                </div>
-                <!-- /.modal-content -->
-              </div>
-              <!-- /.modal-dialog -->
-            </div>
-            <!-- /.modal -->
-            <!-- /Modal tambah data -->
-
-
-          </div>
-          <!-- /.box-header -->
-          <div class="box-body">
-            <table id="example1" class="table table-bordered table-striped">
-              <thead>
-                <tr>
-                  <th>No</th>
-                  <th>Nama Barang</th>
-                  <th>Kategori</th>
-                  <th>Foto</th>
-                  <th>Stok</th>
-                  <th>Harga</th>
-                  <th>Diskon</th>
-                  <th>Deskripsi</th>
-                  <th>Aksi</th>
-                </tr>
-              </thead>
-              <tbody>
-                <?php 
-                $no = 1;
-                $qb = mysqli_query($koneksi, "SELECT * FROM barang");
-                while($db = mysqli_fetch_array($qb)){
-                 ?>
-                <tr>
-                  <td><?=$no++;?></td>
-                  <td><?=$db['nama_barang'];?></td>
-                  <td><?=$db['nama_barang'];?></td>
-                  <td><img width="60px" height="70" src="../img/produk/<?=$db['foto'];?>"></td>
-                  <td><?=$db['stok'];?></td>
-                  <td><?=$db['harga'];?></td>
-                  <td><?=$db['diskon'];?></td>
-                  <td><?=$db['deskripsi'];?></td>
-                  <td>
-                    <button class="btn btn-primary" data-toggle="modal" data-target="#edit1"><i class="fa fa-edit"></i></button>
-                    <a href="#"><button class="btn btn-danger"><i class="fa fa-trash"></i></button></a>
-                  </td>
-                </tr>
-                <!-- Modal Edit data -->
-                <div class="modal fade" id="edit1">
-                  <div class="modal-dialog">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">&times;</span></button>
-                          <h4 class="modal-title">Edit Data Kategori</h4>
-                        </div>
-                        <div class="modal-body">
-                          <form action="" method="post">
-                            <div class="form-group">
-                              <label for="exampleInputEmail1">Email address</label>
-                              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                            </div>
-                            <div class="form-group">
-                              <label for="exampleInputPassword1">Password</label>
-                              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                            </div>
-
-                          </div>
-                          <div class="modal-footer">
-                            <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
-                            <button type="submit" class="btn btn-primary">Simpan</button>
-                          </div>
-                        </form>
-                      </div>
-                      <!-- /.modal-content -->
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Harga</label>
+                      <input type="number" name="harga" class="form-control" placeholder="Masukkan Harga Barang">
                     </div>
-                    <!-- /.modal-dialog -->
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Diskon (jika ada)</label>
+                      <input type="number" name="diskon" class="form-control" value="0">
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Deskripsi</label>
+                      <textarea class="form-control" name="deskripsi"></textarea>
+                    </div>
+
                   </div>
-                  <!-- /.modal -->
-                  <!-- /Modal Edit data -->
-                <?php } ?>
-
-                </tbody>
-              </table>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Simpan</button>
+                  </div>
+                </form>
+              </div>
+              <!-- /.modal-content -->
             </div>
-            <!-- /.box-body -->
+            <!-- /.modal-dialog -->
           </div>
-          <!-- /.box -->
+          <!-- /.modal -->
+          <!-- /Modal tambah data -->
+
+
         </div>
+        <!-- /.box-header -->
+        <div class="box-body">
+          <table id="example1" class="table table-bordered table-striped">
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Nama Barang</th>
+                <th>Kategori</th>
+                <th>Foto</th>
+                <th>Stok</th>
+                <th>Harga</th>
+                <th>Diskon</th>
+                <th>Deskripsi</th>
+                <th>Aksi</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php 
+              $no = 1;
+              $qb = mysqli_query($koneksi, "SELECT * FROM barang JOIN kategori ON barang.id_kategori=kategori.id_kategori");
+              while($db = mysqli_fetch_array($qb)){
+               ?>
+               <tr>
+                <td><?=$no++;?></td>
+                <td><?=$db['nama_barang'];?></td>
+                <td><?=$db['nama_kategori'];?></td>
+                <td><img width="60px" height="70" src="../img/produk/<?=$db['foto'];?>"></td>
+                <td><?=$db['stok'];?></td>
+                <td><?=$db['harga'];?></td>
+                <td><?=$db['diskon'];?>%</td>
+                <td><?=substr($db['deskripsi'], 0, 30);?>...</td>
+                <td>
+                  <button class="btn btn-primary" data-toggle="modal" data-target="#edit<?=$no;?>"><i class="fa fa-edit"></i></button>
+                  <a onclick="return confirm('Hapus barang!')" href="proses.php?proses=delete_barang&&id_barang=<?=$db['id_barang'];?>"><button class="btn btn-danger"><i class="fa fa-trash"></i></button></a>
+                </td>
+              </tr>
+              <!-- Modal Edit data -->
+              <div class="modal fade" id="edit<?=$no;?>">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title">Edit Data Barang</h4>
+                      </div>
+                      <div class="modal-body">
+                        <form action="proses.php?proses=edit_barang" method="POST" enctype="multipart/form-data">
+                          <div class="form-group">
+                            <label for="exampleInputEmail1">Nama Barang</label>
+                            <input type="hidden" name="id_barang" value="<?=$db['id_barang'];?>">
+                            <input type="text" name="nama_barang" class="form-control" value="<?=$db['nama_barang'];?>">
+                          </div>
+                          <div class="form-group">
+                            <label for="exampleInputEmail1">Kategori</label>
+                            <select name="id_kategori" class="form-control">
+                              <option value="0">--Pilih Kategori--</option>
+                              <?php 
+                              include '../koneksi.php';
+                              $query = mysqli_query($koneksi, "SELECT * FROM kategori");
+                              while($dk = mysqli_fetch_array($query)){
+                               ?>
+                               <option <?php if($db['id_kategori']==$dk['id_kategori']){echo "selected='selected'";} ?> value="<?=$dk['id_kategori'];?>"><?=$dk['nama_kategori'];?></option>
+                             <?php } ?>
+                           </select>
+                         </div>
+                         <div class="form-group">
+                          <label for="exampleInputEmail1">Foto</label>
+                          <input type="file" name="foto" value="<?=$db['foto'];?>" class="form-control">
+                        </div>
+                        <div class="form-group">
+                          <label for="exampleInputEmail1">Stok</label>
+                          <input type="number" name="stok" class="form-control" value="<?=$db['stok'];?>">
+                        </div>
+                        <div class="form-group">
+                          <label for="exampleInputEmail1">Harga</label>
+                          <input type="number" name="harga" class="form-control" value="<?=$db['harga'];?>">
+                        </div>
+                        <div class="form-group">
+                          <label for="exampleInputEmail1">Diskon</label>
+                          <input type="number" name="diskon" class="form-control" value="<?=$db['diskon'];?>">
+                        </div>
+                        <div class="form-group">
+                          <label for="exampleInputEmail1">Deskripsi</label>
+                          <textarea class="form-control" name="deskripsi"><?=$db['deskripsi'];?></textarea>
+                        </div>
+
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Edit</button>
+                      </div>
+                    </form>
+                  </div>
+                  <!-- /.modal-content -->
+                </div>
+                <!-- /.modal-dialog -->
+              </div>
+              <!-- /.modal -->
+              <!-- /Modal Edit data -->
+            <?php } ?>
+
+          </tbody>
+        </table>
       </div>
-    </section>
+      <!-- /.box-body -->
+    </div>
+    <!-- /.box -->
+  </div>
+</div>
+</section>
--- admin/proses.php
+++ admin/proses.php
@@ -24,4 +24,22 @@ else if($proses == 'add_barang'){
 	mysqli_query($koneksi,"INSERT INTO barang (id_kategori, nama_barang, foto, stok, deskripsi, harga, diskon) VALUES ('$_POST[id_kategori]', '$_POST[nama_barang]', '$nama_file', '$_POST[stok]', '$_POST[deskripsi]', '$_POST[harga]', '$_POST[diskon]')");
 	header('location:index.php?page=produk');
 }
+else if($proses == 'edit_barang'){
+	$lokasi_file	=$_FILES['foto']['tmp_name'];
+	$tipe_file		=$_FILES['foto']['type'];
+	$nama_file		=$_FILES['foto']['name'];
+	move_uploaded_file($lokasi_file, "../img/produk/$nama_file");
+
+	if(empty($nama_file)){
+		mysqli_query($koneksi,"UPDATE barang SET id_kategori = '$_POST[id_kategori]', nama_barang = '$_POST[nama_barang]', stok = '$_POST[stok]', deskripsi = '$_POST[deskripsi]', harga = '$_POST[harga]', diskon = '$_POST[diskon]' WHERE id_barang = '$_POST[id_barang]'");	
+	}
+	else{
+		mysqli_query($koneksi,"UPDATE barang SET id_kategori = '$_POST[id_kategori]', nama_barang = '$_POST[nama_barang]', foto = '$nama_file', stok = '$_POST[stok]', deskripsi = '$_POST[deskripsi]', harga = '$_POST[harga]', diskon = '$_POST[diskon]' WHERE id_barang = '$_POST[id_barang]'");
+	}
+	header('location:index.php?page=produk');
+}
+else if($proses == 'delete_barang'){
+	mysqli_query($koneksi, "DELETE FROM barang WHERE id_barang = '$_GET[id_barang]'");
+	header('location:index.php?page=produk');
+}
 ?>
